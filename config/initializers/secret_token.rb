8bb0059b879b60d69acfb6bd493162d47c73b31a
Steve Quatrani <spq2461@gmail.com>
2013-01-31 19:38:16 -0500

    first commit for failboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Failboard::Application.config.secret_token = '5afb26ce938557058e1b3114b6afbd58c09bc4d825924122a7a877b4db2b5cddcc40d9a99596320eba5ac2dd535859b5aad2f96d098cdb7b6d1a966f2bdf595d'
