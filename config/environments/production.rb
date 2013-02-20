dd2bbbeba4c5760d18f37c44e09073f0b1be06be
Steve Quatrani <spq2461@gmail.com>
2013-02-20 18:54:26 -0500

    updating form to try to get it working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Failboard::Application.configure do
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AKIAIILBLZ2OJVD56EIQ'],
+    :secret_access_key => ENV['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']
   }
 }
 end
